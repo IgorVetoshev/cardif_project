f216504a23994b621f20189e26b9c2a78b695aaf
IgorVetoshev <igor.vetoshev@gmail.com>
2020-01-29 11:18:36 +0300

    65th commit

--- custom_multiclass.py
+++ custom_multiclass.py
@@ -288,7 +288,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=30, #changed
-                layers='4+')
+                layers='3+')
     
     model_path = 'mask_rcnn_weights' + '.h5'   #changed
     model.keras_model.save_weights(model_path) #changed
