7f336a82de11cc1ab63186511f84df456bda3124
IgorVetoshev <igor.vetoshev@gmail.com>
2020-02-10 19:53:23 +0300

    120th commit

--- mask_rcnn_damage_detection/custom.py
+++ mask_rcnn_damage_detection/custom.py
@@ -74,8 +74,8 @@ class CustomConfig(Config):
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
 
-    # Skip detections with < 90% confidence
-    DETECTION_MIN_CONFIDENCE = 0.9
+    # Skip detections with < 70% confidence
+    DETECTION_MIN_CONFIDENCE = 0.7
 
 
 ############################################################
